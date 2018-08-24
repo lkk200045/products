50dd5d67e10a570fefcdccb779fdfce25723171c
Huang Steve <lkk200045@gmail.com>
2018-08-24 17:05:18 +0800

    add products

--- account.py
+++ account.py
@@ -7,5 +7,7 @@ while True :
 		price = input('請輸入價格:')
 		products.append([goods ,price])
 
-for i in range(len(products)) :
-	print(products[i])
+for i in products:
+	#每一個i裡的0是商品名1是價格
+	print('商品名:',i[0] , '價格', i[1])
+
