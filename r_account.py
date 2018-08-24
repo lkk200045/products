ac2360bcac79f2f668b0c730779f32ec0c593dc1
Huang Steve <lkk200045@gmail.com>
2018-08-24 20:36:49 +0800

    add products

--- r_account.py
+++ r_account.py
@@ -1,8 +1,17 @@
-p = []
-with open("products.csv",'r',encoding='utf-8') as f:
-	for item in f:
-		if '商品,價格' in item:
-			continue
-		name, price = item.strip().split(',')
-		p.append([name,price])
-print(p)
+import os
+
+if os.path.isfile('products.csv'):
+	print('yeah')
+	p = []
+	with open("products.csv",'r',encoding='utf-8') as f:
+		for item in f:
+			if '商品,價格' in item:
+				continue
+			name, price = item.strip().split(',')
+			p.append([name,price])
+	print(p)
+
+else:
+	print('not in computer')
+
+
