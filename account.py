7667b5c963141c33ff5aef8b76020c0a596ce8a0
Huang Steve <lkk200045@gmail.com>
2018-08-24 19:58:57 +0800

    add products

--- account.py
+++ account.py
@@ -7,6 +7,7 @@ while True :
 		price = input('請輸入價格:')
 		products.append([goods ,price])
 
-with open("products.csv",'w') as f:
+with open("products.csv",'w',encoding='utf-8') as f:
+	f.write('商品,價格\n')
 	for i in products:
 		f.write(i[0] + ',' + i[1]  + '\n')
